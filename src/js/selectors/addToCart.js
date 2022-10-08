cf643d971cdc4e4a39cc2b2db05caf76851b7eb6
fernando-ui <fst-96@hotmail.com>
2022-10-07 19:37:01 -0500

    style of thumbnail images

--- src/js/selectors/addToCart.js
+++ src/js/selectors/addToCart.js
@@ -1,3 +1,6 @@
+const multiplyElements = document.querySelector("#multiplyElements");
+const total = document.querySelector("#total");
+
 export const addToCart = () => {
   document.querySelector("#addCartButton").addEventListener("click", () => {
     const totalItemsCart = document.querySelector("#totalItemsCart");
@@ -11,5 +14,8 @@ export const addToCart = () => {
     }
     totalItemsCart.classList.remove = "nav__nav-iconHidden";
     totalItemsCart.innerHTML = totalOfItems;
+
+    multiplyElements.innerText = parseInt(totalItemsCart.innerText);
+    total.innerText = `$${parseInt(totalItemsCart.innerText) * 125}.00`;
   });
 };
