7cd6d20c00c33f8363a7334fa5a4b1ccb866c0f5
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-10-10 12:47:33 -0500

    Add de selected funcion of the images

--- src/js/selectors/selectPreviewImg.js
+++ src/js/selectors/selectPreviewImg.js
@@ -1,5 +1,7 @@
 export const selectedImagePreview = () => {
-    document.querySelectorAll(".previewImage__thumbnailImg").forEach((element) => {
+  document
+    .querySelectorAll(".previewImage__thumbnailImg")
+    .forEach((element) => {
       const reg = /-\d+/g;
       element.addEventListener("click", ({ target }) => {
         const selectedImg = target.src.match(reg);
@@ -10,26 +12,31 @@ export const selectedImagePreview = () => {
         selectedImageStyle(selectedNumber);
       });
     });
-  };
-  const selectedImageStyle = (idImage = 1) => {
+};
+const selectedImageStyle = (idImage) => {
   // * Removing Classes
-    document
-      .querySelectorAll(".previewImage__thumbnailContainer")
-      .forEach((element) => {
-        element.classList.remove("previewImage__thumbnailContainer");
-      });
-    document.querySelectorAll(".previewImage__thumbnailImg").forEach((element) => {
+  document
+    .querySelectorAll(".previewImage__thumbnailContainer")
+    .forEach((element) => {
+      element.classList.remove("previewImage__thumbnailContainer");
+      element.classList.remove("previewImage__thumbnailContainer-selected");
+    });
+  document
+    .querySelectorAll(".previewImage__thumbnailImg")
+    .forEach((element) => {
       element.classList.remove("previewImage__thumbnailImg-selected");
     });
-    
+
   // * Adding classes of the element selected
-    document
-      .querySelector(`.previewImage__thumbnailContainer-${idImage}`)
-      .classList.add("previewImage__thumbnailContainer");
-    document
-      .querySelector(`.previewImage__thumbnailImg-${idImage}`)
-      .classList.add("previewImage__thumbnailImg-selected");
-  };
-  
-  
-  
+  document
+    .querySelector(`.previewImage__thumbnailContainer-${idImage}`)
+    .classList.add("previewImage__thumbnailContainer");
+  document
+    .querySelector(`.previewImage__thumbnailContainer-${idImage}`)
+    .classList.add("previewImage__thumbnailContainer-selected");
+  document
+    .querySelector(`.previewImage__thumbnailImg-${idImage}`)
+    .classList.add("previewImage__thumbnailImg-selected");
+};
+
+// previewImage__thumbnailContainer-selected
